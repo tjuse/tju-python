b272a047e9d0af67b1b4bf4c25e28500fc996ea8
SUPER <superpung.lee@gmail.com>
2023-09-14 23:25:16 +0800

    chore: init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+import tju
+
+setup(
+    name="tju",
+    version=tju.__version__,
+    author="Super Lee",
+    author_email="hi@repus.me",
+    description="The Python TJU client for humans.",
+    url="https://github.com/superpung/tju",
+    packages=['tju']
+)
