76a6f5d9c7bf2a3483b383a19b6f36457f6aee10
SUPER <superpung.lee@gmail.com>
2024-09-25 19:31:21 +0800

    test: profile schema

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+from tju.models.profile import Profile
 from tju.models.schedule import ScheduleCourse
 
 
@@ -17,3 +18,15 @@ def test_schedule_schema():
         if "credit" in raw_dict:
             raw_dict["credit"] = float(raw_dict["credit"])
         assert all(getattr(schedule_course, k) == v for k, v in raw_dict.items())
+
+
+def test_profile_schema():
+    raw_dict = json.loads(
+        Path(__file__)
+        .parent.joinpath("resources/response/parsed_profile_ug.json")
+        .read_text()
+    )
+    profile = Profile(**raw_dict)
+    assert profile is not None
+    for k, v in raw_dict.items():
+        assert getattr(profile, k) == v
